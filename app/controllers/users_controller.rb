a02df062c7509fa366ed842e3a939316b30fba37
Apprentice <apprentice@devbootcamp.com>
2013-04-29 15:59:52 -0700

    pagination

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,11 @@
 class UsersController < ApplicationController
   def index
     
-    User.benchmark('querying user karma points') do
-      @users = User.by_karma.limit(50)
-    end
+    # User.benchmark('querying user karma points') do
+      
+      @page = params[:page].to_i || 1
+      @users = User.by_karma.limit(50).offset(@page*50)
+    # end
 
   end
 end
